edbb43519361536f7d8cd59d48976459c1c1307a
nebo <nebojsa.kamber@4zida.rs>
2023-09-27 14:28:10 +0200

    Unzip the CSV

--- update.js
+++ update.js
@@ -2,6 +2,7 @@ import {createWriteStream} from 'node:fs';
 import {pipeline} from 'node:stream';
 import {promisify} from 'node:util'
 import fetch from 'node-fetch';
+import admZip from 'adm-zip';
 
 const STREETS_URL = 'https://download.geosrbija.rs/adresniregistar_ulica?username=opendata&password=opendata&format=csv&geom=false';
 const streamPipeline = promisify(pipeline);
@@ -10,3 +11,6 @@ const response = await fetch(STREETS_URL);
 if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
 await streamPipeline(response.body, createWriteStream('./streets.zip'));
+
+const zip = new admZip('./streets.zip');
+zip.extractEntryTo("tmp/data/ready/adresni/ulica-sifarnik.csv", "./", false, true);
