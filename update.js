01d5511a9f4a8c3ba1433d7295aab50697ac1752
nebo <nebojsa.kamber@4zida.rs>
2023-09-27 16:12:42 +0200

    UNDO Stem street title(s) - too big index (500k) and too many matches (1k for 4-letter words)

--- update.js
+++ update.js
@@ -4,8 +4,7 @@ import {promisify} from 'node:util'
 import fetch from 'node-fetch';
 import admZip from 'adm-zip';
 import fs from 'fs';
-import {parse} from 'csv-parse';
-import {stemStreet} from "./alphabetical.js";
+import { parse } from 'csv-parse';
 
 const streamPipeline = promisify(pipeline);
 
@@ -14,52 +13,24 @@ const LOCAL_ZIP = './streets.zip';
 const ZIP_ENTRY = 'tmp/data/ready/adresni/ulica-sifarnik.csv';
 const CSV_FILE = './ulica-sifarnik.csv';
 
-fetch(REMOTE_ZIP).then(response => {
-	if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+const response = await fetch(REMOTE_ZIP);
 
-	streamPipeline(response.body, createWriteStream(LOCAL_ZIP)).then(() => {
-		const zip = new admZip(LOCAL_ZIP);
-		zip.extractEntryTo(ZIP_ENTRY, "./", false, true);
+if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
-		let alphabeticalIndex = new Map();
-		fs.createReadStream(CSV_FILE).pipe(parse({
-			delimiter: ',',
-			columns: true
-		}, function (err, streets) {
-			if (err) {
-				console.warn(err);
-			} else {
-				streets.forEach(street => {
-					const terms = stemStreet(street);
-					terms.forEach(term => {
-						if (alphabeticalIndex.has(term)) {
-							let items = alphabeticalIndex.get(term);
-							items.push(street);
-							alphabeticalIndex.set(term, items);
-						} else {
-							alphabeticalIndex.set(term, [street]);
-						}
-					});
+await streamPipeline(response.body, createWriteStream(LOCAL_ZIP));
 
-					//Remove (currently) redundant properties
-					delete street.streetid;
-					delete street.ulica_maticni_broj;
-					delete street.ulica_ime;
-					delete street.naselje_maticni_broj;
-					delete street.naselje_ime;
-					delete street.opstina_maticni_broj;
-					delete street.opstina_ime;
-					delete street.tip;
-					delete street.tip_lat;
-					delete street.created;
-					delete street.modificationdate;
-					delete street.retired;
-					delete street.primary_key
-				});
+const zip = new admZip(LOCAL_ZIP);
+zip.extractEntryTo(ZIP_ENTRY, "./", false, true);
 
-				console.log(`Alphabetical index built with ${alphabeticalIndex.size} items`);
-				console.log(alphabeticalIndex.get('zmaj'));
-			}
-		}));
-	});
+const parser = parse({
+	delimiter: ',',
+	columns: true
+}, function(err, data){
+	if (err) {
+		console.warn(err);
+	} else {
+		console.log(data);
+	}
 });
+
+fs.createReadStream(CSV_FILE).pipe(parser);
