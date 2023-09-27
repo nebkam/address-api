d4f2a567875d2abbeb6cc437dee2dc5a7b84cf7b
nebo <nebojsa.kamber@4zida.rs>
2023-09-27 14:42:26 +0200

    Read the CSV

--- update.js
+++ update.js
@@ -3,14 +3,30 @@ import {pipeline} from 'node:stream';
 import {promisify} from 'node:util'
 import fetch from 'node-fetch';
 import admZip from 'adm-zip';
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const STREETS_URL = 'https://download.geosrbija.rs/adresniregistar_ulica?username=opendata&password=opendata&format=csv&geom=false';
 const streamPipeline = promisify(pipeline);
-const response = await fetch(STREETS_URL);
+
+const REMOTE_ZIP = 'https://download.geosrbija.rs/adresniregistar_ulica?username=opendata&password=opendata&format=csv&geom=false';
+const LOCAL_ZIP = './streets.zip';
+const ZIP_ENTRY = 'tmp/data/ready/adresni/ulica-sifarnik.csv';
+const CSV_FILE = './ulica-sifarnik.csv';
+
+const response = await fetch(REMOTE_ZIP);
 
 if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
-await streamPipeline(response.body, createWriteStream('./streets.zip'));
+await streamPipeline(response.body, createWriteStream(LOCAL_ZIP));
 
-const zip = new admZip('./streets.zip');
-zip.extractEntryTo("tmp/data/ready/adresni/ulica-sifarnik.csv", "./", false, true);
+const zip = new admZip(LOCAL_ZIP);
+zip.extractEntryTo(ZIP_ENTRY, "./", false, true);
+
+const parser = parse({delimiter: ',', columns: true}, function(err, data){
+	console.log(data);
+});
+
+fs.createReadStream(CSV_FILE).pipe(parser);
