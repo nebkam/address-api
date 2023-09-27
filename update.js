e196e7c2d1a4ad977eff16b5dbc6d24a434514ec
nebo <nebojsa.kamber@4zida.rs>
2023-09-27 13:50:39 +0200

    Download binary (zip)

--- update.js
+++ update.js
@@ -1,6 +1,12 @@
+import {createWriteStream} from 'node:fs';
+import {pipeline} from 'node:stream';
+import {promisify} from 'node:util'
 import fetch from 'node-fetch';
 
-const response = await fetch('https://download.geosrbija.rs/adresniregistar_ulica?username=opendata&password=opendata&format=csv&geom=false');
-const body = await response.text();
+const STREETS_URL = 'https://download.geosrbija.rs/adresniregistar_ulica?username=opendata&password=opendata&format=csv&geom=false';
+const streamPipeline = promisify(pipeline);
+const response = await fetch(STREETS_URL);
 
-console.log(body);
+if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+
+await streamPipeline(response.body, createWriteStream('./streets.zip'));
