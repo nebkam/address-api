26e70eca2d2c692734ff0e913ec2704a71753d84
nebo <nebojsa.kamber@4zida.rs>
2023-09-27 14:42:47 +0200

    Read the CSV

--- update.js
+++ update.js
@@ -5,9 +5,6 @@ import fetch from 'node-fetch';
 import admZip from 'adm-zip';
 import fs from 'fs';
 import { parse } from 'csv-parse';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const streamPipeline = promisify(pipeline);
 
