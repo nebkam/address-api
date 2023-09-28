75d99e042eec7b0f9cfe39e503557d321f336daf
nebo <nebojsa.kamber@4zida.rs>
2023-09-28 08:16:54 +0200

    Cleanup

--- download_streets.js
+++ download_streets.js
@@ -1,4 +1,4 @@
-import {createWriteStream} from 'node:fs';
+import {createWriteStream, unlink} from 'node:fs';
 import {pipeline} from 'node:stream';
 import {promisify} from 'node:util'
 import fetch from 'node-fetch';
@@ -12,9 +12,13 @@ const ZIP_ENTRY = 'tmp/data/ready/adresni/ulica-sifarnik.csv';
 
 const response = await fetch(REMOTE_ZIP);
 
-if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+if (!response.ok) throw new Error(`Unexpected response ${response.statusText}`);
 
 await streamPipeline(response.body, createWriteStream(LOCAL_ZIP));
 
 const zip = new admZip(LOCAL_ZIP);
 zip.extractEntryTo(ZIP_ENTRY, "./", false, true);
+
+unlink(LOCAL_ZIP, (err) => {
+	if (err) throw err;
+});
